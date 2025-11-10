932503bcfbefd7a26b4913232a1fe8e9c633eac9
Erfani <vahid_erfani@hotmail.com>
2025-11-10 16:00:33 +0100

    Initial commit

---
+++ src/Training/Random1.java
@@ -0,0 +1,17 @@
+package Training;
+import java.util.Random;
+import java.util.Scanner;
+public class Random1 {
+
+    static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Random random = new Random();
+
+
+    }
+
+
+
+}
